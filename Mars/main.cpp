5d55a4bef932279d65f1bb334c128417ce14563d
Matthieu <matthieu.zandecki@gmail.com>
2017-04-15 10:58:40 +0200

    Initial Commit

---
+++ Mars/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mars
+//
+//  Created by Matthieu Zandecki on 15/04/2017.
+//  Copyright © 2017 Matthieu Zandecki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
