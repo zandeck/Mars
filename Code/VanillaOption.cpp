e3adcbebda8060023d6d6608fe01e46d85b3841d
Matthieu <matthieu.zandecki@gmail.com>
2017-04-17 14:03:40 +0200

    Add VanillaOption file

---
+++ Code/VanillaOption.cpp
@@ -0,0 +1,9 @@
+//
+//  VanillaOption.cpp
+//  Mars
+//
+//  Created by Matthieu Zandecki on 17/04/2017.
+//  Copyright © 2017 Matthieu Zandecki. All rights reserved.
+//
+
+#include "VanillaOption.hpp"
---
+++ Headers/VanillaOption.hpp
@@ -0,0 +1,14 @@
+//
+//  VanillaOption.hpp
+//  Mars
+//
+//  Created by Matthieu Zandecki on 17/04/2017.
+//  Copyright © 2017 Matthieu Zandecki. All rights reserved.
+//
+
+#ifndef VanillaOption_hpp
+#define VanillaOption_hpp
+
+#include <stdio.h>
+
+#endif /* VanillaOption_hpp */
