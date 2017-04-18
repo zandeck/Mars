baafd212a00b50fe01c5921c0852e84a2f1e7d37
Matthieu <matthieu.zandecki@gmail.com>
2017-04-18 09:59:17 +0200

    Add Statistics files

---
+++ Code/Statistics/StatisticsMC.cpp
@@ -0,0 +1,9 @@
+//
+//  StatisticsMC.cpp
+//  Mars
+//
+//  Created by Matthieu Zandecki on 18/04/2017.
+//  Copyright © 2017 Matthieu Zandecki. All rights reserved.
+//
+
+#include "StatisticsMC.hpp"
---
+++ Headers/Statistics/StatisticsMC.hpp
@@ -0,0 +1,14 @@
+//
+//  StatisticsMC.hpp
+//  Mars
+//
+//  Created by Matthieu Zandecki on 18/04/2017.
+//  Copyright © 2017 Matthieu Zandecki. All rights reserved.
+//
+
+#ifndef StatisticsMC_hpp
+#define StatisticsMC_hpp
+
+#include <stdio.h>
+
+#endif /* StatisticsMC_hpp */
